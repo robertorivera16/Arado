6b7d0a2456ce036cadd53c23ea4dcb35ada06f40
Jesipr <jesi.nieves@gmail.com>
2018-12-10 16:31:30 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ $$(document).on('page:init', '.page[data-name="checkout"]', function (e) {
     del.classList.add("swipeout-delete");
     li.appendChild(swipeoutAct);
 
-    var prod = document.createTextNode(key + "        " + value + " lbs");
+    var prod = document.createTextNode(key.charAt(0).toUpperCase() + key.slice(1) "        " + value + " lbs");
     itemTitle.appendChild(prod);
     swipeoutAct.appendChild(del);
 
